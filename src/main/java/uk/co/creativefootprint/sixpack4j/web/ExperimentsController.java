78d2ed8d9a7b4da90bd87e0bdca488358ef78552
David Carr <davidcarrs@gmail.com>
2016-01-30 00:13:35 +0000

    added experimentsController endpoints

--- src/main/java/uk/co/creativefootprint/sixpack4j/web/ExperimentsController.java
+++ src/main/java/uk/co/creativefootprint/sixpack4j/web/ExperimentsController.java
@@ -3,14 +3,15 @@ package uk.co.creativefootprint.sixpack4j.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import uk.co.creativefootprint.sixpack4j.model.Experiment;
 
 @RestController
 @RequestMapping("/experiments")
 public class ExperimentsController {
 
     @RequestMapping(method = RequestMethod.GET)
-    String getExperiments() {
-        return "blah";
+    Experiment getExperiments() {
+        return new Experiment();
     }
 
 }
